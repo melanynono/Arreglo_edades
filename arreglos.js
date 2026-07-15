33a05a2a40919c96a482163c61b1479e702933b4
MelanyNono <melanynono123@gmail.com>
2026-07-15 13:13:30 -0500

    Agregar funcionalidad para registrar edades

--- arreglos.js
+++ arreglos.js
@@ -5,3 +5,26 @@ const txtEdad = document.getElementById("edad");
 const btnAgregar = document.querySelector(".formulario button");
 const tablaIzquierda = document.getElementById("tablaIzquierda");
 const tablaDerecha = document.getElementById("tablaDerecha");
+
+btnAgregar.addEventListener("click", agregarEdad);
+
+function agregarEdad() {
+    let edad = parseInt(txtEdad.value);
+
+    if (isNaN(edad) || edad <= 0) {
+        alert("Ingrese una edad válida.");
+        return;
+    }
+
+    edadesIzquierda.push(edad);
+    txtEdad.value = "";
+
+    dibujarTablas();
+}
+
+function dibujarTablas() {
+    tablaIzquierda.innerHTML = "";
+    tablaDerecha.innerHTML = "";
+}
+
+dibujarTablas();
