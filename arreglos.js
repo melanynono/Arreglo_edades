8ae916e3b829c9d43edd324aeb0d8e702c86bd2f
MelanyNono <melanynono123@gmail.com>
2026-07-15 13:11:31 -0500

    Inicializar arreglos y obtener elementos del DOM

---
+++ arreglos.js
@@ -0,0 +1,7 @@
+let edadesIzquierda = [];
+let edadesDerecha = [];
+
+const txtEdad = document.getElementById("edad");
+const btnAgregar = document.querySelector(".formulario button");
+const tablaIzquierda = document.getElementById("tablaIzquierda");
+const tablaDerecha = document.getElementById("tablaDerecha");
